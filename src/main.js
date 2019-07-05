182feb5387575e9242da1559369870f2a5c283aa
FatherofDharma <fatherofdharma@gmail.com>
2019-07-05 14:13:16 -0700

    add code to return the office phone number to screen as well.

--- src/main.js
+++ src/main.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
       let results = JSON.parse(response);
       console.log(results);
       $('span.doctor-result').text(`The Doctor's name is: ${results.data[0].profile.first_name} ${results.data[0].profile.last_name} Address: ${results.data[0].practices[0].visit_address.street}
-      ${results.data[0].practices[0].visit_address.zip} Is accepting new patients: ${results.data[0].practices[0].accepts_new_patients}`);
+      ${results.data[0].practices[0].visit_address.zip} Is accepting new patients: ${results.data[0].practices[0].accepts_new_patients} Phone: ${results.data[0].practices[0].phones[0].number}`);
 
     });
   });
