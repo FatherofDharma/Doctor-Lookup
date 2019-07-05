73c65571ac8bdc7a6085044585088db52bc7ed7d
FatherofDharma <fatherofdharma@gmail.com>
2019-07-05 11:37:02 -0700

    add user interface logic to help keep the server from failing at load. --Still trying to resolve some issues with favicon loading in browser.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
 import './styles.css';
 import './doctor.js';
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 
-console.log(process.env.exports.apiKey)
+console.log(process.env.exports.apiKey);
+
+$(document).ready(function () {
+  $('form.doctor-request').submit(function (event) {
+    event.preventDefault();
+    const inputtedFirstName = $('input#first-name').val();
+    const inputtedLastName = $('input#last-name').val();
+    const inputtedMedicalIssue = $('input#medical-issue').val();
+    console.log(inputtedFirstName);
+    console.log(inputtedLastName);
+    console.log(inputtedMedicalIssue);
+    $('input#first-name').val('');
+    $('input#last-name').val('');
+    $('input#medical-issue').val();
+  })
+});
